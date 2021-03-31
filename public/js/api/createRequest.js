9b06a700177c15df8f05b6117977e1b623bbedc4
barsich <barsicchan@gmail.com>
2021-03-31 20:17:12 +0500

    createRequest added

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,37 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  if (!options.data) {
+    return;
+  }
+  
 
+  const xhr = new XMLHttpRequest();
+  let formData;
+
+  if (options.method === 'GET') {
+    options.url += '?';
+    for (let key in options.data) {
+      options.url += `${key}=${options.data[key]}&`;
+    }
+    options.url -= '&';
+  } else {
+    formData = new FormData();
+    for (let key in options.data) {
+      formData.append(key, options.data[key]);
+    }
+  }
+
+  try {
+    xhr.open(options.method, options.url);
+    xhr.send(formData);
+    xhr.addEventListener('readystatechange', () => {
+      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        options.callback(response.error, response);
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
