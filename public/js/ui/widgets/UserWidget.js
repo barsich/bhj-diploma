65218c97fe1e43627877528f1c9a5cad6edc40e9
barsich <barsicchan@gmail.com>
2021-04-03 22:18:12 +0500

    UserWidget added

--- public/js/ui/widgets/UserWidget.js
+++ public/js/ui/widgets/UserWidget.js
@@ -11,8 +11,11 @@ class UserWidget {
    * Если переданный элемент не существует,
    * необходимо выкинуть ошибку.
    * */
-  constructor(element){
-
+  constructor(element) {
+    if (!element) {
+      throw new Error('Передан пустой элемент!');
+    }
+    this.element = element;
   }
 
   /**
@@ -22,7 +25,9 @@ class UserWidget {
    * в элемент .user-name устанавливает имя
    * авторизованного пользователя
    * */
-  update(){
-
+  update() {
+    if (User.current()) {
+      this.element.querySelector('.user-name').innerText = User.current().name;
+    }
   }
 }
